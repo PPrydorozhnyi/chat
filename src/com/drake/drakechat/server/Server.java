e5a8330810100238043065ead6e3eec6306ca87d
Peter Pridorozhny <Peter35128@gmail.com>
2017-09-28 22:22:52 +0300

    Server logic is added

--- src/com/drake/drakechat/server/Server.java
+++ src/com/drake/drakechat/server/Server.java
@@ -1,12 +1,62 @@
 package com.drake.drakechat.server;
 
-public class Server {
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
-private int port;
+public class Server implements Runnable {
+
+	private DatagramSocket socket;
+	private int port;
+	private boolean running = false;
+	private Thread run;
+	private Thread manage;
+	private Thread send;
+	private Thread receive;
 	
 	public Server(int port) {
 		
 		this.port = port;
+		
+		try {
+			socket = new DatagramSocket();
+		} catch(SocketException e) {
+			e.printStackTrace();
+		}
+		run = new Thread(this, "Server");
+	}
+	
+	public void run() {
+		running = true;
+		
+		manageClients();
+		receive();
+		
+	}
+	
+	private void manageClients() {
+		manage = new Thread("Manage") {
+			@Override
+			public void run() {
+				while (running){
+					// managing
+				}
+			}
+		};
+		
+		manage.start();
+	}
+	
+	private void receive() {
+		
+		receive = new Thread("Receve") {
+			@Override
+			public void run() {
+				while (running) {
+					
+				}
+			}
+		};
+		
 	}
 	
 }
